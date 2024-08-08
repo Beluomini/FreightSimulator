b144ca6374dc3ccfe23e20bf139d4b43bdf3bcf3
Beluomini <lucasbeluomini1@gmail.com>
2024-08-08 14:35:05 -0300

    feat(prisma): create initial database schema with Prisma, database name is "ship-sim-db"

---
+++ backend/prisma/migrations/20240808172908_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "logistic-operator" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "weightCost" DOUBLE PRECISION NOT NULL,
+    "distanceMult" DOUBLE PRECISION NOT NULL,
+    "distanceMult100" DOUBLE PRECISION NOT NULL,
+    "distanceMult500" DOUBLE PRECISION NOT NULL,
+    "deliveryTime" DOUBLE PRECISION NOT NULL,
+    "deliveryTime100" DOUBLE PRECISION NOT NULL,
+    "deliveryTime500" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "logistic-operator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "simulation" (
+    "id" TEXT NOT NULL,
+    "clientName" TEXT NOT NULL,
+    "fromAddress" TEXT NOT NULL,
+    "toAddress" TEXT NOT NULL,
+    "productHeight" DOUBLE PRECISION NOT NULL,
+    "productWidth" DOUBLE PRECISION NOT NULL,
+    "productDepth" DOUBLE PRECISION NOT NULL,
+    "productWeight" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "simulation_pkey" PRIMARY KEY ("id")
+);
