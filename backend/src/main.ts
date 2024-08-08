0690ca247f7ddd471458e35771b383387e8f06cf
Beluomini <lucasbeluomini1@gmail.com>
2024-08-08 10:06:03 -0300

    feat: start swagger documentation on /docs route

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function freightAPI() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Freight Simulator')
+    .setDescription('Freight Simulator API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on port: ${process.env.PORT}`);
 }
