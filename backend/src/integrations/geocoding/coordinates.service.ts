29d75d97077701dcc690686fad4129afea7343d9
Beluomini <lucasbeluomini1@gmail.com>
2024-08-14 23:38:55 -0300

    feat(coordinates): Creation of the coordinate service for consumption by the geocoding API

---
+++ backend/src/integrations/geocoding/coordinates.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class CoordinatesService {
+  private readonly geocodingApiKey = process.env.GEOCODING_API_KEY;
+  private readonly geocodingRoute = process.env.GEOCODING_API_ROUTE;
+
+  async getCoordinates(address: string) {
+    const coordinates = await axios.get(this.geocodingRoute, {
+      params: {
+        key: this.geocodingApiKey,
+        address: address,
+      },
+    });
+    return coordinates.data.results[0].geometry.location;
+  }
+}
