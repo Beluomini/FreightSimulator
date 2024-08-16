914748e965fb13baead97d81e226592818cbf326
Beluomini <lucasbeluomini1@gmail.com>
2024-08-15 22:43:43 -0300

    feat(coordinate DTO): create a class to describle the coordinates

---
+++ backend/src/integrations/geocoding/coordinates.dto.ts
@@ -0,0 +1,4 @@
+export class ResponseCoordenatesDto {
+  lat: number;
+  lng: number;
+}
--- backend/src/integrations/geocoding/coordinates.service.ts
+++ backend/src/integrations/geocoding/coordinates.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { ResponseCoordenatesDto } from './coordinates.dto';
 
 @Injectable()
 export class CoordinatesService {
   private readonly geocodingApiKey = process.env.GEOCODING_API_KEY;
   private readonly geocodingRoute = process.env.GEOCODING_API_ROUTE;
 
-  async getCoordinates(address: string) {
+  async getCoordinates(address: string): Promise<ResponseCoordenatesDto> {
     const coordinates = await axios.get(this.geocodingRoute, {
       params: {
         key: this.geocodingApiKey,
