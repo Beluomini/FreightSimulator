ed2ffb0bc052413ba76a0b34de7e2b175264a28f
Beluomini <lucasbeluomini1@gmail.com>
2024-08-17 15:17:17 -0300

    feat(handle address): create the process address function to prepare the addresses to API req

---
+++ frontend/src/utils/handleAdress.ts
@@ -0,0 +1,29 @@
+export function handleAdress(
+  address: string,
+  number1: string,
+  neighborhood1: string,
+  city1: string,
+  state1: string,
+  cep1: string,
+  country1: string,
+) {
+  let addressStr =
+    address +
+    "%2C%20" +
+    number1 +
+    "%2C%20" +
+    neighborhood1 +
+    "%2C%20" +
+    city1 +
+    "%2C%20" +
+    state1 +
+    "%2C%20" +
+    cep1 +
+    "%2C%20" +
+    country1;
+  const processAddressStr = addressStr
+    .replace(" ", "%20")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  return processAddressStr;
+}
