825d986d18d89b1b7402bcccabc9d53750029880
Lucas Beluomini <lucasbeluomini1@gmail.com>
2024-08-21 14:18:20 +0000

    feat(geocoding exceptions): handling errors from external geocoding api

--- backend/src/integrations/geocoding/coordinates.service.ts
+++ backend/src/integrations/geocoding/coordinates.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import axios from 'axios';
 import { ResponseCoordenatesDto } from './coordinates.dto';
 
@@ -8,12 +15,31 @@ export class CoordinatesService {
   private readonly geocodingRoute = process.env.GEOCODING_API_ROUTE;
 
   async getCoordinates(address: string): Promise<ResponseCoordenatesDto> {
-    const coordinates = await axios.get(this.geocodingRoute, {
-      params: {
-        key: this.geocodingApiKey,
-        address: address,
-      },
-    });
-    return coordinates.data.results[0].geometry.location;
+    try {
+      const coordinates = await axios.get(this.geocodingRoute, {
+        params: {
+          key: this.geocodingApiKey,
+          address: address,
+        },
+      });
+      if (coordinates.data.status === 'OK') {
+        return coordinates.data.results[0].geometry.location;
+      } else if (coordinates.data.status === 'ZERO_RESULTS') {
+        throw new NotFoundException('Address not found');
+      } else if (coordinates.data.status === 'REQUEST_DENIED') {
+        throw new BadRequestException('Request denied');
+      } else if (coordinates.data.status === 'INVALID_REQUEST') {
+        throw new BadRequestException('Invalid request');
+      } else if (coordinates.data.status === 'OVER_QUERY_LIMIT') {
+        throw new HttpException(
+          'Your key has exceeded its rate limit or is invalid.',
+          HttpStatus.TOO_MANY_REQUESTS,
+        );
+      } else {
+        throw new InternalServerErrorException('Error getting coordinates');
+      }
+    } catch (error) {
+      throw new InternalServerErrorException('Error getting coordinates');
+    }
   }
 }
