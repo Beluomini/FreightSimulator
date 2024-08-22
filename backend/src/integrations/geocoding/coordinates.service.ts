fb4c820d41e71019a4271ffb15d9081d4dd2c75e
Beluomini <lucasbeluomini1@gmail.com>
2024-08-21 22:03:22 -0300

    feat(goecoding exceptions): handle external API exceptions

--- backend/src/integrations/geocoding/coordinates.service.ts
+++ backend/src/integrations/geocoding/coordinates.service.ts
@@ -1,10 +1,7 @@
 import {
-  BadRequestException,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
-  HttpStatus,
-  HttpException,
 } from '@nestjs/common';
 import axios from 'axios';
 import { ResponseCoordenatesDto } from './coordinates.dto';
@@ -26,20 +23,11 @@ export class CoordinatesService {
         return coordinates.data.results[0].geometry.location;
       } else if (coordinates.data.status === 'ZERO_RESULTS') {
         throw new NotFoundException('Address not found');
-      } else if (coordinates.data.status === 'REQUEST_DENIED') {
-        throw new BadRequestException('Request denied');
-      } else if (coordinates.data.status === 'INVALID_REQUEST') {
-        throw new BadRequestException('Invalid request');
-      } else if (coordinates.data.status === 'OVER_QUERY_LIMIT') {
-        throw new HttpException(
-          'Your key has exceeded its rate limit or is invalid.',
-          HttpStatus.TOO_MANY_REQUESTS,
-        );
       } else {
         throw new InternalServerErrorException('Error getting coordinates');
       }
     } catch (error) {
-      throw new InternalServerErrorException('Error getting coordinates');
+      throw error;
     }
   }
 }
