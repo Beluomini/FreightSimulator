d884e5d87a286f59ed5e7c945079d838e86df1ac
Beluomini <lucasbeluomini1@gmail.com>
2024-08-21 21:02:20 -0300

    feat(infinite scroll): creation of frontend logic for infinite scrolling of simulations; error handling for failure in fetching data from the API

--- frontend/src/components/simulationList.tsx
+++ frontend/src/components/simulationList.tsx
@@ -1,86 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { SimulationDto } from "../types/simulation";
-
-const mockedSimulations = [
-  {
-    id: "1",
-    clientName: "Client 1",
-    productHeight: 1.0,
-    productLength: 1.0,
-    productWidth: 1.0,
-    toAddress: "Address 1",
-    fromAddress: "Address 2",
-  },
-  {
-    id: "2",
-    clientName: "Client 2",
-    productHeight: 2.0,
-    productLength: 2.0,
-    productWidth: 2.0,
-    toAddress: "Address 2",
-    fromAddress: "Address 3",
-  },
-  {
-    id: "3",
-    clientName: "Client 3",
-    productHeight: 3.0,
-    productLength: 3.0,
-    productWidth: 3.0,
-    toAddress: "Address 3",
-    fromAddress: "Address 4",
-  },
-  {
-    id: "3",
-    clientName: "Client 3",
-    productHeight: 3.0,
-    productLength: 3.0,
-    productWidth: 3.0,
-    toAddress: "Address 3",
-    fromAddress: "Address 4",
-  },
-  {
-    id: "3",
-    clientName: "Client 3",
-    productHeight: 3.0,
-    productLength: 3.0,
-    productWidth: 3.0,
-    toAddress: "Address 3",
-    fromAddress: "Address 4",
-  },
-  {
-    id: "3",
-    clientName: "Client 3",
-    productHeight: 3.0,
-    productLength: 3.0,
-    productWidth: 3.0,
-    toAddress: "Address 3",
-    fromAddress: "Address 4",
-  },
-];
+import { useInView } from "react-intersection-observer";
 
 export default function SimulationList() {
   const [data, setData] = useState<SimulationDto[]>([]);
   const [loading, setLoading] = useState(true);
+  const [finished, setFinished] = useState(false);
   const [error, setError] = useState("");
+  const { ref, inView } = useInView();
+
+  const [counter, setCounter] = useState(0);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const getSimulations = async () => {
+    try {
+      const response = await axios.get(
+        process.env.NEXT_PUBLIC_API_URL + "/simulation/ten?index=" + counter,
+      );
+      if (response.data.data.length === 0) {
+        setFinished(true);
+        return;
+      }
+      setData([...data, ...response.data.data]);
+      setCounter(counter + 1);
+    } catch (error) {
+      setError("Erro ao buscar dados de simulções antigas!");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          process.env.NEXT_PUBLIC_API_URL + "/simulation",
-        );
-        setData(response.data.data);
-      } catch (error) {
-        setData(mockedSimulations);
-        setError(""); //Erro ao buscar dados de simulções antigas
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+    getSimulations();
+    if (inView) {
+      getSimulations();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
 
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>{error}</p>;
@@ -90,7 +47,10 @@ export default function SimulationList() {
       <h2 className="text-xl font-semibold text-gray-800 mb-4">
         List of Simulations
       </h2>
-      <div className="flex flex-col gap-4 overflow-y-auto scrollbar-thin">
+      <div
+        ref={divRef}
+        className="flex flex-col gap-4 overflow-y-auto scrollbar-thin"
+      >
         {data.map((simulation) => (
           <div key={simulation.id} className="flex flex-col p-2">
             <div className="flex flex-wrap">
@@ -104,17 +64,33 @@ export default function SimulationList() {
             </div>
             <div className="flex flex-wrap">
               <p>
-                <strong>Address From:</strong> {simulation.fromAddress}
+                <strong>Address From:</strong>{" "}
+                {simulation.fromAddress
+                  .replaceAll("%20", " ")
+                  .replaceAll("%2C", ",")}
               </p>
             </div>
             <div className="flex flex-wrap">
               <p>
-                <strong>Address To:</strong> {simulation.toAddress}
+                <strong>Address To:</strong>{" "}
+                {simulation.toAddress
+                  .replaceAll("%20", " ")
+                  .replaceAll("%2C", ",")}
               </p>
             </div>
             <hr className="border-t border-gray-300 mt-2"></hr>
           </div>
         ))}
+        {finished && (
+          <div ref={ref} className="flex items-center justify-center">
+            End of the simulations list
+          </div>
+        )}
+        {!finished && (
+          <div ref={ref} className="flex items-center justify-center">
+            Loading...
+          </div>
+        )}
       </div>
     </div>
   );
